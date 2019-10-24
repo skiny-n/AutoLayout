a728cd11c17e0e90d765f2e0a38e28e0c346bcee
Stanislav Novacek <stanislav.novacek@elevup.com>
2019-10-24 11:00:54 +0200

    Added SPM support

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AutoLayout",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "AutoLayout", targets: ["AutoLayout"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "AutoLayout",
+            path: "./AutoLayout/"
+        )
+    ]
+)
