e0f4b9142a644a6be9c4d77ef5ffb5ba3a9c38af
Stanislav Novacek <stanislav.novacek@elevup.com>
2020-01-14 11:07:41 +0100

    Added convenience typealiases for AutoLayout<UIView> and AutoLayout<UILayoutGuide>

---
+++ AutoLayout/Sources/AutoLayout/Typealiases.swift
@@ -0,0 +1,15 @@
+//
+//  Typealiases.swift
+//  AutoLayout
+//
+//  Created by Stanislav Novacek on 14/01/2020.
+//
+
+import Foundation
+import UIKit
+
+/// Typealias for `AutoLayout<UIView>`
+public typealias UIViewLayout = AutoLayout<UIView>
+
+/// Typealias for `AutoLayout<UILayoutGuide>`
+public typealias UILayoutGuideLayout = AutoLayout<UILayoutGuide>
